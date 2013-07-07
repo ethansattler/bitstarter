ab0ed961ab776579c45b71d057972be724419cf2
Ethan Sattler <mcdasethan@gmail.com>
2013-07-07 10:51:02 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var text = fs.readFileSync('index.html','utf8')
+
+var strings = buf.toString(text)
+
 app.get('/', function(request, response) {
-  response.send('test');
+  response.send(strings);
 });
 
 var port = process.env.PORT || 5000;
