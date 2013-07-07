3ae7219bce64fa18a4a6bba09984550421d5d164
Ethan Sattler <mcdasethan@gmail.com>
2013-07-07 10:45:13 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-});
+var data = fs.readFileSync('index.html','utf8');
 
 
 app.get('/', function(request, response) {
